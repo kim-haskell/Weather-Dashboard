69699787c76581c638547b404078f3693023d298
Kim Haskell <kim.haskell03@gmail.com>
2020-07-09 23:10:28 -0600

    adding styles.css

--- script.js
+++ script.js
@@ -4,41 +4,53 @@ $(document).ready(function(){
         var searchValue = $("#search-value").val();
         $("search-value").val("");
         searchWeather(searchValue);
+
     });
     $(".list-group history").on("click", "li", function(){
 
     });
 
+    function makeRow(text) {
+        searchWeather($(this).text());
+    }
+
     function searchWeather(searchValue) {
-    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + searchValue + "&appid=a13b9c3cca609f2bad9924f130ee8841";
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + searchValue + "&appid=a13b9c3cca609f2bad9924f130ee8841&units=imperial";
         $.ajax({
           url: queryURL, 
           method: "GET",
           dataType: "JSON", 
           success: function(response){
-               console.log(response.wind.speed)
-            var wind = $("<p>").addClass("card-text").text("wind-speed: " + response.wind.speed + "mph");
-            var temp = $("<p>").addClass("card-text").text("temperature: " + response.main.temp + "Farenheit");
-            var humidity = $("<p>").addClass("card-text").text("humidity: " + response.main.humidity + "%");
+               //console.log(response.wind.speed)
+            var wind = $("<p>").addClass("card-text").text("Wind-speed: " + response.wind.speed + " mph");
+            var temp = $("<p>").addClass("card-text").text("Temperature: " + response.main.temp + " Fahrenheit");
+            var humidity = $("<p>").addClass("card-text").text("Humidity: " + response.main.humidity + " %");
             var uvIndex = $("<p>").addClass("card-text").text("UV Index: " + response.main.uvIndex)
             
-           $(".card-text").text(response);
+           $("#today").append(searchValue);
+           $("#today").append(temp);
+           $("#today").append(wind);
+           $("#today").append(humidity);
+           $("#today").append(uvIndex);
 
           }
         });
     }
 
-    function makeRow(text) {
-        searchWeather($(this).text());
-    }
-
    function getForecast(searchValue) {
-       $.ajax({
-         url: queryURL,
+       
+        var queryURL2 = "https://api.openweathermap.org/data/2.5/forecast?q=" + searchValue + "&appid=a13b9c3cca609f2bad9924f130ee8841&units=imperial";
+        $.ajax({
+        url: queryURL2,
          method: "GET",
-       }).then(function (response){
-           makeRow(response);
-       });
+         dataType: "JSON",
+         success: function(data){
+             console.log(data.main.temp)
+             var day1 = $("<div>").addClass("card-forecast").text("Temperature: " + data.main.temp + " Fahrenheit");
+
+             $("#forecast").append(day1);
+         }
+       })
    }
     var history = JSON.parse(window.localStorage.getItem("history")) || [];
 
