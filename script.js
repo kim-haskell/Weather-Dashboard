326249d761e848b46dcd1694f2fb8a9191350e6a
Kim Haskell <kim.haskell03@gmail.com>
2020-07-09 21:40:53 -0600

    adding js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
-    
     $("#search-button").on("click", function(){
+
         var searchValue = $("#search-value").val();
         $("search-value").val("");
         searchWeather(searchValue);
@@ -8,40 +8,45 @@ $(document).ready(function(){
     $(".list-group history").on("click", "li", function(){
 
     });
-    function makeRow(text) {
-        searchWeather($(this).text());
-    }
-    
-    
+
     function searchWeather(searchValue) {
-    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + searchValue + "&appid=a13b9c3cca609f2bad9924f130ee8841";
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + searchValue + "&appid=a13b9c3cca609f2bad9924f130ee8841";
         $.ajax({
           url: queryURL, 
           method: "GET",
           dataType: "JSON", 
           success: function(response){
-    
+               console.log(response.wind.speed)
             var wind = $("<p>").addClass("card-text").text("wind-speed: " + response.wind.speed + "mph");
             var temp = $("<p>").addClass("card-text").text("temperature: " + response.main.temp + "Farenheit");
             var humidity = $("<p>").addClass("card-text").text("humidity: " + response.main.humidity + "%");
             var uvIndex = $("<p>").addClass("card-text").text("UV Index: " + response.main.uvIndex)
+            
+           $(".card-text").text(response);
 
-           $("card-text").text(response);
           }
         });
     }
-   // function getForecast(searchValue) {
-       // $.ajax({
 
-      //  });
-  //  }
-   // var history = JSON.parse(window.localStorage.getItem("history")) || [];
+    function makeRow(text) {
+        searchWeather($(this).text());
+    }
 
-    //if (history.length > 0) {
-      //  searchWeather(history[history.length-1]);
-    //}
-    //for (var i = 0; i < history.length; i++){
-     //   makeRow(history[i]);
-   // }
+   function getForecast(searchValue) {
+       $.ajax({
+         url: queryURL,
+         method: "GET",
+       }).then(function (response){
+           makeRow(response);
+       });
+   }
+    var history = JSON.parse(window.localStorage.getItem("history")) || [];
+
+    if (history.length > 0) {
+        searchWeather(history[history.length-1]);
+    }
+    for (var i = 0; i < history.length; i++){
+        makeRow(history[i]);
+    }
     
     })
